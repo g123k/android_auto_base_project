5bce38672b2d773d2b6c2140ec1cc04d0f9d08fc
Edouard Marquez <g123k@users.noreply.github.com>
2022-04-27 17:55:44 +0200

    first commit

---
+++ automotive/src/main/java/com/example/androidauto/AppSession.kt
@@ -0,0 +1,13 @@
+package com.example.androidauto
+
+import android.content.Intent
+import androidx.car.app.Screen
+import androidx.car.app.Session
+
+class AppSession : Session() {
+
+    override fun onCreateScreen(intent: Intent): Screen {
+        return HelloWorldScreen(carContext)
+    }
+
+}
---
+++ automotive/src/main/java/com/example/androidauto/HelloWorldScreen.kt
@@ -0,0 +1,17 @@
+package com.example.androidauto
+
+import androidx.car.app.CarContext
+import androidx.car.app.Screen
+import androidx.car.app.model.*
+
+class HelloWorldScreen(carContext: CarContext) : Screen(carContext) {
+
+    override fun onGetTemplate(): Template {
+        val row = Row.Builder().setTitle("Hello World!").build()
+
+        return PaneTemplate.Builder(Pane.Builder().addRow(row).build())
+            .setHeaderAction(Action.APP_ICON)
+            .build()
+    }
+
+}
